6ccabb21122e78e9be6cd28b3255330cb8f0c748
muhammedhm <143387754+muhammedhm@users.noreply.github.com>
2023-12-20 12:12:17 +0500

    Add files via upload

--- http_request.c
+++ http_request.c
@@ -1,69 +1,52 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <curl/curl.h>
 #include <stdlib.h>
 
-
 struct MemoryStruct {
   char *memory;
   size_t size;
 };
- 
-static size_t
-WriteMemoryCallback(void *contents, size_t size, size_t nmemb, 
-                    void *userp)
-{
-  size_t realsize = size * nmemb;
-  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
-  
-  char *ptr = realloc(mem->memory, mem->size + realsize + 1);
-  if(!ptr) 
-  {
-    printf("not enough memory (realloc returned NULL)\n");
-    return 0;
+
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+  FILE *file = fopen("response.txt", "a");
+  if (file == NULL) {
+    fprintf(stderr, "Error opening file for writing\n");
+    return 0; // Signal an error
   }
- 
-  mem->memory = ptr;
-  memcpy(&(mem->memory[mem->size]), contents, realsize);
-  mem->size += realsize;
-  mem->memory[mem->size] = 0;
- 
-  return realsize;
+
+  size_t real_size = size * nmemb;
+  for (size_t i = 0; i < nmemb; ++i) {
+    fprintf(file, "%c", ((char *)contents)[i]);
+  }
+
+  fclose(file);
+
+  return nmemb;
 }
 
-int main(void)
-{
+int main(void) {
   CURL *curl;
   CURLcode result;
 
-  struct MemoryStruct chunk; 
-  chunk.memory = malloc(1);
-  chunk.size = 0;
-
   curl = curl_easy_init();
-  if (curl == NULL)
-  {
-    fprintf(stderr, "HTTP request failed\n");
+  if (curl == NULL) {
+    fprintf(stderr, "Failed to initialize libcurl\n");
     return -1;
   }
 
   curl_easy_setopt(curl, CURLOPT_URL, "https://api.openweathermap.org/data/2.5/weather?q=Karachi&appid=7b825f37458912ba5b22873e78b1fc33");
 
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk); 
 
   result = curl_easy_perform(curl);
 
-  if (result != CURLE_OK)
-  {
+  if (result != CURLE_OK) {
     fprintf(stderr, "Error: %s\n", curl_easy_strerror(result));
-    return -1;
   }
 
-  printf("%s <--- response string\n", chunk.memory);
-
-  free(chunk.memory);
   curl_easy_cleanup(curl);
- 
+
   return 0;
 }
